eb77797b6352a3f5445c412d9b9ad7972ac27677
adev05 <andrew.nasibulin@mail.ru>
2024-10-06 21:48:27 +0300

    updates

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -130,6 +130,9 @@ export const authOptions: NextAuthOptions = {
 				})
 			}
 		},
+		async session(message) {
+			console.log('session message', message)
+		},
 	},
 }
 export default NextAuth(authOptions)
